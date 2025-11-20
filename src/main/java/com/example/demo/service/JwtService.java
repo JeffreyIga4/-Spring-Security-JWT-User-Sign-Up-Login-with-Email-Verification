6524617b933eac84318b9ce32032d9c0318fb491
Jeffrey Igala <jeffreyigala@MacBookPro.station>
2025-11-20 16:36:04 +0000

    JwtService completed

--- src/main/java/com/example/demo/service/JwtService.java
+++ src/main/java/com/example/demo/service/JwtService.java
@@ -6,11 +6,11 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import java.security.Key;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
