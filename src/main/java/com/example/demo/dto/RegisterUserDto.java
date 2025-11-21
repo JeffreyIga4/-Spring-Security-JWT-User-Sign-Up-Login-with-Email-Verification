60e433f8c36c68704e18191ca8c349215a6c7cf6
Jeffrey Igala <jeffreyigala@MacBookPro.station>
2025-11-21 17:55:58 +0000

    Register User Dto file created

---
+++ src/main/java/com/example/demo/dto/RegisterUserDto.java
@@ -0,0 +1,10 @@
+package com.example.demo.dto;
+/* store all the different data transfer objects */
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterUserDto {
+}
