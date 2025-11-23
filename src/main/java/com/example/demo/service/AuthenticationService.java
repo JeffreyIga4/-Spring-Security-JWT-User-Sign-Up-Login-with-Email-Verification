e51491ec226bdcbac60e54006b25896a7a4dbaa1
Jeffrey Igala <jeffreyigala@MacBookPro.station>
2025-11-23 17:28:28 +0000

    Authentication file for users created

---
+++ src/main/java/com/example/demo/service/AuthenticationService.java
@@ -0,0 +1,29 @@
+package com.example.demo.service;
+/* contains the logic for authenticating the existing users and any new users */
+
+import com.example.demo.config.JwtAuthenticationFilter;
+import com.example.demo.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final EmailService emailService;
+
+    public AuthenticationService(
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder,
+            AuthenticationManager authenticationManager,
+            EmailService emailService
+    ) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.emailService = emailService;
+    }
+
+}
